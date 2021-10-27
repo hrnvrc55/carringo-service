5a96d741c4a56c569011c8d93e165bcad21f9ec8
harun <harun.varici@teksenbilisim.com>
2021-10-27 09:20:45 +0300

    gg

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,2 +1,6 @@
 export const apiUrl = 'https://api.parcapaketi.com/api';
-//export const apiUrl = 'http://localhost:21021/api'
+//export const apiUrl = 'http://localhost:21021/api';
+
+// export const messageApi = 'http://localhost:8080';
+
+
